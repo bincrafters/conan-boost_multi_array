65c2ad9eefa83e3817ff777a1c302fb999eb1fad
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:16 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostMulti_ArrayConan(ConanFile):
     name = "Boost.Multi_Array"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/multi_array"
+    url = "https://github.com/bincrafters/conan-boost-multi_array"
+    source_url = "https://github.com/boostorg/multi_array"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "multi_array"
@@ -23,7 +23,7 @@ class BoostMulti_ArrayConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
