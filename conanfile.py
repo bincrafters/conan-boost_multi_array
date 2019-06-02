462061d4b491bd9aaa669ff475bc3fb05fa9597d
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,23 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostMulti_ArrayConan(base.BoostBaseConan):
     name = "boost_multi_array"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_multi_array"
-    lib_short_names = ["multi_array"]
-    header_only_libs = ["multi_array"]
-    b2_requires = [
-        "boost_array",
-        "boost_assert",
-        "boost_concept_check",
-        "boost_config",
-        "boost_core",
-        "boost_functional",
-        "boost_iterator",
-        "boost_mpl",
-        "boost_static_assert",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
